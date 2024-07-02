26d76b57c86c44f4fc070e85f9f33d1b85a0c089
Dongwoo Choi <cdw8431@gmail.com>
2024-07-03 01:19:37 +0900

    Title: 최고의 33위치, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240703/최고의 33위치/best-place-of-33.py
+++ 240703/최고의 33위치/best-place-of-33.py
@@ -1,14 +1,15 @@
 n = int(input())
+size = 3
 
 box = []
 for _ in range(n):
     box.append([int(i) for i in input().split()])
 
 max_num = 0
-for idx in range(n - 3) or [0]:
+for idx in range(n - size) or [0]:
     sum_num = 0
-    for row in range(n):
-        for col in range(n):
+    for row in range(size):
+        for col in range(size):
             if box[row + idx][col + idx]:
                 sum_num += 1
     if sum_num > max_num:
