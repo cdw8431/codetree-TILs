db49770b9881eafb05b60e3f48a3183d2cd9c63f
Dongwoo Choi <cdw8431@gmail.com>
2024-07-05 23:15:52 +0900

    Title: 금 채굴하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240705/금 채굴하기/gold-mining.py
@@ -0,0 +1,27 @@
+n, m = [int(num) for num in input().split()]
+matrix = [[int(num) for num in input().split()] for _ in range(n)]
+
+max_k = int((n - 1) / 2)
+
+def mining_gold(row, col, k):
+    gold = 0
+    for i in range(n):
+        for j in range(n):
+           if abs(row - i) + abs(col - j) <= k and matrix[i][j] == 1:
+                gold += 1
+    return gold
+
+max_gold = 0
+
+for row in range(n):
+    for col in range(n):
+        for k in range(max_k):
+            gold = mining_gold(row, col, k+1)
+
+            pay = k * k + (k + 1) * (k + 1)
+            gold_price = gold * m
+
+            if pay <= gold_price and gold > max_gold:
+                max_gold = gold
+
+print(max_gold)
