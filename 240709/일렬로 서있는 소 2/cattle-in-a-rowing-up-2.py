267f5f88767368840d14d24abaaf321c78a538c2
Dongwoo Choi <cdw8431@gmail.com>
2024-07-09 00:50:41 +0900

    Title: 일렬로 서있는 소 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240709/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240709/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ match_count = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if cow_heights[i] < cow_heights[j] < cow_heights[k]:
+            if cow_heights[i] <= cow_heights[j] <= cow_heights[k]:
                 match_count += 1
 
 print(match_count)
